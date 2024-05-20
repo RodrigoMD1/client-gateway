6cf18e30cb51265b32e0dffce79f267cf0dcc5c1
rodrigo <rodrigo.martinez224@gmail.com>
2024-05-20 03:22:50 -0300

    v3

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,4 @@
+export * from './services'
+
 export * from './envs'
+
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { PRODUCT_SERVICE, envs } from 'src/config';
 
 @Module({
   controllers: [ProductsController],
   providers: [],
+  imports: [
+    ClientsModule.register([
+
+      {
+        // aca tiene que tener la misma conexion que en productos para la forma en la que van a hablar los dos  en el main del otro proyecto lo encontras con el nombre COX1 en la parte de envs.ts tambien hay de esto pero es la configuracion de envs
+        name: PRODUCT_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          host: envs.productsMicroserviceHost,
+          port: envs.port
+
+        }
+
+
+
+      },
+
+    ]),
+  ]
 })
 export class ProductsModule { }
