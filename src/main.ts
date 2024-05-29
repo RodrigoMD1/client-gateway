f3be9952c8f1e6664e99e39cd540833bde6c3137
rodrigo <rodrigo.martinez224@gmail.com>
2024-05-29 04:16:46 -0300

    v5

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
 
 async function bootstrap() {
@@ -10,6 +10,14 @@ async function bootstrap() {
 
 
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
 
   app.setGlobalPrefix('api')
 
