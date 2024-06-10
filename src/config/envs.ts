025315a7600464b4f63c686e7a834d1f5f433ea5
rodrigo <rodrigo.martinez224@gmail.com>
2024-06-10 07:01:05 -0300

    v9

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -6,30 +6,39 @@ interface EnvVars {
   PORT: number;
   PRODUCTS_MICROSERVICE_HOST: string;
   PRODUCTS_MICROSERVICE_PORT: number; // este tiene que tener el mismo port que el otro proyecto 
+  ORDERS_MICROSERVICE_HOST: string;
+  ORDERS_MICROSERVICE_PORT: number;
 }
 
 const envsSchema = joi.object({
   PORT: joi.number().required(),
   PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
   PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
+  ORDERS_MICROSERVICE_HOST: joi.string().required(),
+  ORDERS_MICROSERVICE_PORT: joi.number().required()
 
 })
-.unknown(true);
+  .unknown(true);
 
-const { error, value } = envsSchema.validate( process.env );
+const { error, value } = envsSchema.validate(process.env);
 
 
-if ( error ) {
-  throw new Error(`Config validation error: ${ error.message }`);
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
 }
 
-const envVars:EnvVars = value;
+const envVars: EnvVars = value;
 
 
 export const envs = {
   port: envVars.PORT,
   productsMicroserviceHost: envVars.PRODUCTS_MICROSERVICE_HOST,
   productsMicroservicePort: envVars.PRODUCTS_MICROSERVICE_PORT,
+
+
+  ordersMicroserviceHost: envVars.ORDERS_MICROSERVICE_HOST,
+  ordersMicroservicePort: envVars.ORDERS_MICROSERVICE_PORT,
+
 };
 
 ////////////////////////////////////
---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+
+import { ORDER_SERVICE } from 'src/config';
+import { UpdateOrderDto } from './dto/update-order.dto';
+import { CreateOrderDto } from './dto/create-order.dto';
+
+
+@Controller()
+export class OrdersController {
+  constructor(
+    @Inject(ORDER_SERVICE) private readonly ordersClient: ClientProxy,
+  ) { }
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.ordersClient.send('createOrder', createOrderDto);
+
+  }
+
+  @Get()
+  findAll() {
+    return this.ordersClient.send('findAllOrders', {});
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.ordersClient.send('findOneOrder', { id });
+  }
+
+
+}
