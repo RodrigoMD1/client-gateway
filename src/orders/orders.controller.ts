2dfeb076b936dedbd924958260b90da05f606eab
RodrigoDuvivier <rodrigo.martinez224@gmail.com>
2024-06-20 21:02:58 -0300

    v7

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -3,8 +3,8 @@ import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 
 import { ORDER_SERVICE } from 'src/config';
-import { UpdateOrderDto } from './dto/update-order.dto';
-import { CreateOrderDto } from './dto/create-order.dto';
+
+import { CreateOrderDto } from './dto';
 
 
 @Controller()
