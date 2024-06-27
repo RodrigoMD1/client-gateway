e14df8f8272614c074be4d8ccde28b2dffd51c3d
RodrigoDuvivier <rodrigo.martinez224@gmail.com>
2024-06-27 15:23:15 -0300

    v10

--- src/orders/dto/index.ts
+++ src/orders/dto/index.ts
@@ -1,3 +1,8 @@
 
 
+
 export * from './create-order.dto';
+export * from './order-pagination.dto';
+export * from './status.dto';
+
+
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,14 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
-
+import { Body, Controller, Get, Inject, Param, ParseUUIDPipe, Patch, Post, Query } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { ORDER_SERVICE } from 'src/config';
+import { CreateOrderDto, OrderPaginationDto, StatusDto } from './dto';
+import { firstValueFrom } from 'rxjs';
+import { PaginationDto } from 'src/common';
 
-import { CreateOrderDto } from './dto';
 
 
-@Controller()
+@Controller('orders')
 export class OrdersController {
+
   constructor(
     @Inject(ORDER_SERVICE) private readonly ordersClient: ClientProxy,
   ) { }
@@ -20,14 +22,56 @@ export class OrdersController {
   }
 
   @Get()
-  findAll() {
-    return this.ordersClient.send('findAllOrders', {});
+  findAll(@Query() orderpaginationDto: OrderPaginationDto) {
+    return this.ordersClient.send('findAllOrders', orderpaginationDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ordersClient.send('findOneOrder', { id });
+  @Get('id/:id')
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+
+    try {
+
+      const order = await firstValueFrom(
+        this.ordersClient.send('findOneOrder', { id })
+      );
+      return order;
+
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
+
+
+  @Get(':status')
+  async findOneByStatus(
+    @Param() statusDto: StatusDto,
+    @Query() paginationDto: PaginationDto,
+  ) {
+    try {
+      return this.ordersClient.send('findAllOrders', {
+        ...paginationDto,
+        status: statusDto.status,
+      });
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+
+
+
+  @Patch(':id')
+  //Aca puedo poner los guards para la autorizacion o autenticacion para que los que tengan permiso puedan cambiar el estado de pendiente a cancelado o aprobado etc o guards globales poner
+  changeStatus(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() statusDto: StatusDto
+  ) {
+    try {
+      return this.ordersClient.send('changeOrderStatus', { id, status: statusDto.status })
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
 
 }
